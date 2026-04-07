7f378b1bc0e07eef0334e4908bbb3ea042bf20a4
Open Source Maintainer <opensource@example.invalid>
2026-04-06 23:45:30 -0300

    fix(youtube-music): increase playlists panel to 40%

--- src/modules/youtube-music/components/library/library-view.tsx
+++ src/modules/youtube-music/components/library/library-view.tsx
@@ -43,7 +43,7 @@ export function LibraryView({
   return (
     <ResizablePanelGroup orientation="horizontal" className="h-full">
       {/* Left column: Playlists */}
-      <ResizablePanel defaultSize={30} minSize={30} maxSize={30}>
+      <ResizablePanel defaultSize={40} minSize={40} maxSize={40}>
         <div className="flex h-full flex-col">
           <div className="shrink-0 px-4 pt-4 pb-2">
             <SectionHeader title="Playlists" />
@@ -89,7 +89,7 @@ export function LibraryView({
       <ResizableHandle />
 
       {/* Right column: Curtidas */}
-      <ResizablePanel defaultSize={70} minSize={70}>
+      <ResizablePanel defaultSize={60} minSize={60}>
         <div className="flex h-full flex-col">
           <div className="shrink-0 px-4 pt-4 pb-2">
             <SectionHeader title="Curtidas" />
