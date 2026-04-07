599b700b44f3b3fa875af2faa6807f50eda4fdf0
Open Source Maintainer <opensource@example.invalid>
2026-04-06 23:39:08 -0300

    fix(youtube-music): remove artistas+álbuns panel from library, keep curtidas + playlists only

--- src/modules/youtube-music/components/library/library-view.tsx
+++ src/modules/youtube-music/components/library/library-view.tsx
@@ -10,15 +10,11 @@ import {
   AvatarImage,
   AvatarFallback,
 } from "@/components/ui/avatar";
-import { CarouselSection } from "../shared/carousel-section";
-import { MediaCard } from "../shared/media-card";
 import { SectionHeader } from "../shared/section-header";
 import { VirtualTrackList } from "../shared/virtual-track-list";
 import {
   mockTracks,
   mockPlaylists,
-  mockArtists,
-  mockAlbums,
 } from "../../mock/data";
 import type { Track, StackPage } from "../../types/music";
 
@@ -46,90 +42,39 @@ export function LibraryView({
 
   return (
     <ResizablePanelGroup orientation="horizontal" className="h-full">
-      {/* Left column: Curtidas (top) + Artistas & Álbuns (bottom) */}
+      {/* Left column: Curtidas */}
       <ResizablePanel defaultSize={70} minSize={40}>
-        <ResizablePanelGroup orientation="vertical" className="h-full">
-          {/* Top-left: Curtidas */}
-          <ResizablePanel defaultSize={60} minSize={25}>
-            <div className="flex h-full flex-col">
-              <div className="shrink-0 px-4 pt-4 pb-2">
-                <SectionHeader title="Curtidas" />
-              </div>
-              <ScrollArea
-                ref={curtidasAreaRef}
-                className="flex-1 overflow-hidden"
-              >
-                <div className="px-4 pb-4">
-                  <VirtualTrackList
-                    tracks={mockTracks}
-                    scrollElementRef={curtidasScrollRef}
-                    scrollMargin={0}
-                    onPlay={onPlayTrack}
-                    onAddToQueue={onAddToQueue}
-                    onGoToArtist={(id) =>
-                      onNavigate({ type: "artist", artistId: id })
-                    }
-                    onGoToAlbum={(id) =>
-                      onNavigate({ type: "album", albumId: id })
-                    }
-                  />
-                </div>
-              </ScrollArea>
+        <div className="flex h-full flex-col">
+          <div className="shrink-0 px-4 pt-4 pb-2">
+            <SectionHeader title="Curtidas" />
+          </div>
+          <ScrollArea
+            ref={curtidasAreaRef}
+            className="flex-1 overflow-hidden"
+          >
+            <div className="px-4 pb-4">
+              <VirtualTrackList
+                tracks={mockTracks}
+                scrollElementRef={curtidasScrollRef}
+                scrollMargin={0}
+                onPlay={onPlayTrack}
+                onAddToQueue={onAddToQueue}
+                onGoToArtist={(id) =>
+                  onNavigate({ type: "artist", artistId: id })
+                }
+                onGoToAlbum={(id) =>
+                  onNavigate({ type: "album", albumId: id })
+                }
+              />
             </div>
-          </ResizablePanel>
-
-          <ResizableHandle />
-
-          {/* Bottom-left: Artistas + Álbuns */}
-          <ResizablePanel defaultSize={40} minSize={20}>
-            <ScrollArea className="h-full">
-              <div className="space-y-4 p-4">
-                <CarouselSection title="Artistas que você segue">
-                  {mockArtists.map((artist) => (
-                    <MediaCard
-                      key={artist.browseId}
-                      title={artist.name}
-                      subtitle={artist.subscribers}
-                      thumbnails={artist.thumbnails}
-                      rounded="full"
-                      onClick={() =>
-                        onNavigate({
-                          type: "artist",
-                          artistId: artist.browseId,
-                        })
-                      }
-                    />
-                  ))}
-                </CarouselSection>
-
-                <CarouselSection title="Álbuns salvos">
-                  {mockAlbums.slice(0, 5).map((album) => (
-                    <MediaCard
-                      key={album.browseId}
-                      title={album.title}
-                      subtitle={album.artists
-                        .map((a) => a.name)
-                        .join(", ")}
-                      thumbnails={album.thumbnails}
-                      onClick={() =>
-                        onNavigate({
-                          type: "album",
-                          albumId: album.browseId,
-                        })
-                      }
-                    />
-                  ))}
-                </CarouselSection>
-              </div>
-            </ScrollArea>
-          </ResizablePanel>
-        </ResizablePanelGroup>
+          </ScrollArea>
+        </div>
       </ResizablePanel>
 
       <ResizableHandle />
 
       {/* Right column: Playlists */}
-      <ResizablePanel defaultSize={30} minSize={20}>
+      <ResizablePanel defaultSize={30} minSize={15}>
         <div className="flex h-full flex-col">
           <div className="shrink-0 px-4 pt-4 pb-2">
             <SectionHeader title="Playlists" />
